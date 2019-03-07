0ef4662d522d2970f5613c63dd3a62c03df0d27e
morecofe <morecoffee42@gmail.com>
2019-03-07 18:46:19 +0300

    initial

---
+++ README.md
@@ -0,0 +1,93 @@
+## arena.sh CLI
+
+<img src="https://media.giphy.com/media/EO7fp3wfl3HBIRySR8/giphy.gif" width="500">
+
+A CLI browser for open-source arena shooter games.
+
+## Platforms and games
+Tested to be working on MacOSX, Linux and Windows/Cygwin. For Cygwin, make sure to install all three packages below. 
+
+When a game is not found, user will be asked to download the official unified zip wherever available and "after installation notes" specific to the platform will be shown (for e.g. you need to install OpenAL-soft at Linux or place its dll files in Windows). 
+
+## Supported games
+
+Currently, following games are tested to be working on all platforms:
+
+  - Open Arena (unified zip available)
+  - Xonotic (unified zip available)
+  - QuakeWorld
+  - Alien Arena (osx support pending)
+
+
+## How to install
+
+Requirements:
+
+- python2 or later
+- curl
+- unzip (optional)
+
+You can download the bash script as follows and start using:
+
+```
+$ curl https://raw.githubusercontent.com/arena-sh/cli/master/arena.sh -O
+$ chmod +x arena.sh
+```
+
+## Quick start
+
+Here are valid examples with some of their short-forms:
+
+```
+$ ./arena.sh game:openarena type:ffa +humans
+$ ./arena.sh defrag game:xonotic
+$ ./arena.sh game:quakeworld type:duel +he +hf
+$ ./arena.sh g:qw t:duel +he +hf       # short-form
+$ ./arena.sh player:sarge
+$ ./arena.sh p:sarge       # short-form
+```
+
+QuakeWorld duel spot demonstration:
+
+<img src="https://media.giphy.com/media/xkF2OOpDN6AWotY2bP/giphy.gif" width="450">
+
+## Usage
+Any string will search in server names and player. However, you can utilize filters and flags below to construct the search queries.
+
+### Filters
+| filter | short-form | description | examples
+| ------ | ------ | ------ | ------|
+| game: | g: | game title or short code | xonotic, xo, alienarena, aa, quakeworld, qw, openarena, oa |
+| type: | t: | game type | ffa, ctf, duel, ca |
+| map: | map: | game map | ztn, ztndm3, stormkeep |
+| mod: | mod: | game mod | aftershock, rat, ktx, fortress |
+| player: | p: | player name | metalbot, sarge |
+
+### Conditions and sorting
+| name | description |
+| ------ | ------ |
+| +humans | have at least 1 human playing |
+| +nearby | sort by nearby, default when +humans is applied |
+| +he | hide empty |
+| +hf | hide full |
+
+## Auto downloader (experimental)
+If a launcher command fails to run, the official unified zip (for available games only) will be asked to be downloaded:
+
+<img src="https://media.giphy.com/media/YVNq7mSCmm95yJ9Ham/giphy.gif" width="450">
+
+Downloaded games will be unzipped at `$HOME` for Linux, `/Applications` at MacOSX and `C:\games` for Windows. 
+
+It's good idea to pay attention for the "after installation" notes followed by the `unzip`. They are also accessible at any game/server web page as well (press v to open). 
+
+
+## How does it compare to qstat?
+Qstat is a complete tool for querying server masters and querying each server afterwards. It can be considered as a full-scan solution. CLI tool provided here, use http://arena.sh/ as its backend so it doesn't hit any servers. In order to find the game you're looking for, all search is done within a single `curl` command from a readily populated list. As it's service-backed, it only consist around 150 lines of bash, that means no compilation or installation is needed. Service approach also provides additional benefits of a powerful query language, rule set analysis and hiding of broken/fake servers. 
+
+
+## Development & Reporting
+When making a change, mind your best to keep the change compatible with all three platforms. If the problem is within the launcher command, please leave a <a href="https://arena.sh/feedback/">feedback</a> instead!
+
+## Debugging
+In case there is an unzip/run issue (even after applying installation notes presented at any server page at the game), to save bandwith, you can comment out the `curl` command and comment in `cp` operation at next line. Just make sure to grab gamefiles.zip and copy it to your home directory before losing the tmp location created by `mktemp`. 
+
---
+++ arena.sh
@@ -0,0 +1,170 @@
+#!/bin/bash
+
+API_ENDPOINT="https://arena.sh"
+
+if ! [ -x "$(command -v curl)" ]; then
+    echo 'Error: curl is not installed.' >&2
+    exit 1
+fi
+
+if ! [ -x "$(command -v python)" ]; then
+    echo 'Error: python is not installed.' >&2
+    exit 1
+fi
+
+export CLICOLOR=1
+export LSCOLORS=GxFxCxDxBxegedabagaced
+
+OS="n/a"
+case "$(uname -s)" in
+   Darwin) OS="macosx" ;;
+   Linux) OS="linux" ;;
+   CYGWIN*|MINGW32*|MSYS*) OS="windows" ;;
+esac
+
+query=""
+current_page="1"
+total_pages="1"
+fetch_results() {
+    page=$1
+    shift
+    query="${@}"
+
+    if (( page < 1  )); then
+        page="1"
+    fi
+    if (( page > total_pages )); then
+        page="$total_pages"
+    fi
+
+    response=$(curl -fGs "$API_ENDPOINT/cli/search/" --data-urlencode "q=$query" --data-urlencode "p=$page")
+    if [ -z "$response" ]
+    then
+        echo "Missing input or server is unavailable."
+        exit 1
+    fi
+    results=$(echo "$response" | python -c "import sys, json; print(json.load(sys.stdin)['results'])")
+    total_pages=$(echo "$response" | python -c "import sys, json; print(json.load(sys.stdin)['total_pages'])")
+    current_page=$(echo "$response" | python -c "import sys, json; print(json.load(sys.stdin)['current_page'])")
+    if [ -z "$results" ]
+    then
+        echo "No results found."
+        exit 0
+    fi
+}
+fetch_results 1 ${@}
+
+view_results() {
+ clear
+ echo -e "\033[1;32m[Search results for $query]\033[0m"
+ echo -e "\033[1mKey\tGame\tType\tPlayers\tLoc\tName\033[0m"
+ COUNTER=1
+ echo "$results" | while IFS= read -r line ; do echo -e "${COUNTER}\t${line}" | sed -E "s/(.*);#;.*/\1/";COUNTER=$[$COUNTER +1]; done
+ echo -e "page: $current_page/$total_pages\n"
+ echo -ne "p = prev, n = next, q = quit"
+ echo -ne "\n$1"
+ while read -n1 -r -p "choose a server by using a number key" && [[ $REPLY != q && $REPLY != " " ]]; do
+   case $REPLY in
+     p) clear;fetch_results $[$current_page-1] $query; view_results;break;;
+     n) clear;fetch_results $[$current_page+1] $query; view_results;break;;
+   esac
+   clear
+   viewdetails $REPLY
+   break
+ done
+}
+
+viewdetails() {
+   ORDER=$1
+   game_id=$(echo "$results" | sed -n "${ORDER}p" | sed -E "s/.*;#;(.*)/\1/")
+
+   if [ -z "$game_id" ]
+   then
+      view_results "unknown game, please choose an existing line\n"
+   else
+      clear
+      game_info=$(curl -Gs "$API_ENDPOINT/cli/game/$game_id/" --data-urlencode "os=$OS")
+      chart=$(echo $game_info | python -c "import sys, json; print(json.load(sys.stdin)['chart'])")
+      game_cmd=$(echo $game_info | python -c "import sys, json; print(json.load(sys.stdin)['cmd'])")
+      echo -ne "\033[1;32m[Server information]\033[0m\n"
+      echo -ne "$chart"
+      echo -ne "\n\n"
+      echo -ne "\033[1;32m[Command to launch and join]\033[0m\n"
+      if [ -z "$game_cmd" ]
+      then
+          echo -ne "Currently unavailable for your platform, but we're working hard to bring the support!"
+      else
+          echo -ne "$game_cmd"
+      fi
+      echo -ne "\n\n"
+      while read -n1 -r -p "r = run, v = web, b = back, q = quit" && [[ $REPLY != q && $REPLY != " " ]]; do
+        echo -ne "\n"
+        case $REPLY in
+         b) clear;view_results;break;;
+         v) python -mwebbrowser "$API_ENDPOINT/game/$game_id/";continue;;
+         r)
+           cmd_to_run="$game_cmd"
+           #cmd_to_run="echo 'No such file'" #testing purposes only, this should still act like game exists as we only capture stderr below
+           if [ "$OS" = "windows" ]; then
+               echo "$game_cmd" > cmd.ps1
+               cmd_to_run="powershell -executionpolicy bypass -file cmd.ps1"
+           fi
+           { run_stderr=$(eval $cmd_to_run 2>&1 >&3 3>&-); } 3>&1
+           not_found=$(echo "$run_stderr" | head -n1 | grep -av "shared lib" | grep -a "No such file\|cannot be run" | tr -d '\040\011\012\015')
+           if [ -z "$not_found" ] 
+           then
+               echo -ne "$run_stderr"
+           else
+               echo "Game not found, checking for an unified zip..."
+               game_code=$(echo "$game_info" | python -c "import json,sys;obj=json.load(sys.stdin); print obj['game_code']")
+               dl_info=$(curl -Gs "$API_ENDPOINT/cli/game-dl/$game_code/" --data-urlencode "os=$OS")
+               dl_link=$(echo $dl_info | python -c "import sys, json; print(json.load(sys.stdin)['dl_link'])")
+               dl_notes=$(echo $dl_info | python -c "import sys, json; print(json.load(sys.stdin)['after_install'])")
+               if [ -z "$dl_link" ] 
+               then
+                   echo "Unified zip unavailable, please follow installation instructions at the server page (press v to open)" 
+               else
+                 read -n1 -r -p "Continue to download and install? [Y/n]";
+                 case $REPLY in
+                     Y) echo -ne "\nDownloading $dl_link..\n"
+                         if ! [ -x "$(command -v unzip)" ]; then
+                             echo 'Error: unzip is not installed. Please install it first so we can extract the zip file after the download.' >&2
+                             exit 1
+                         fi
+                         tmpdir=$(dirname $(mktemp -u))
+                         curl $dl_link -J -L -o $tmpdir/gamefiles.zip
+                         #cp ~/gamefiles.zip $tmpdir/  #keep for local dev testing
+                         dest=$HOME/games/
+                         if [ "$OS" = "macosx" ]; then
+                             dest=/Applications/games/
+                         elif [ "$OS" = "windows" ]; then
+                             dest="/cygdrive/C/games/"
+                         fi
+                         mkdir -p $dest
+                         unzip $tmpdir/gamefiles.zip -d $dest
+                         chmod -R 777 $dest
+                         echo -ne "\n\nInstalled to $dest\n"
+                         if ! [ -z "$dl_notes" ]; then
+                             echo -ne "\n$dl_notes\n"
+                             exit 0
+                         else
+                             continue
+                         fi
+                         echo -ne "\n"
+                         continue
+                     ;;
+                     *) echo -ne "\n"
+                 esac
+               fi
+           fi
+         ;;
+        esac
+        echo -ne "\n"
+      done
+   fi
+
+}
+
+view_results
+
+echo -e "\n"
